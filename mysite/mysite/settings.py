5bcac0293414c8e3c184a88c8b54208e633d3f42
aryansj <aryanjunagade5@gmail.com>
2021-09-16 12:26:31 +0530

    final commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite'
 ]
 
 MIDDLEWARE = [
@@ -126,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amritansh131001@outlook.com'
-EMAIL_HOST_PASSWORD = 'Trospy123'
+
+
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.shortcuts import render
 
 def hello(request):
-    send_mail(
-        'Subject here',
-        'We did it. END OF PROJECt',
-        'amritansh131001@outlook.com',
-        ['aryanjunagade5@gmail.com'],
-        fail_silently=False,
-    )
-    return HttpResponse("Hello baby!")
+    if request.method == "POST":
+        subject = request.POST['subject']
+        message = request.POST['content']
+        recipients = [request.POST['recipient']]
+        send_mail(subject, message, request.POST['sender'], recipients, False, request.POST['sender'], request.POST['sender_password'])
+    return render(request, "mysite/index.html")
     
